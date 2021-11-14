796cf7dcf1e45f3768f1915c2b83612ae2459ce1
Laura Dunne <laura@lauradunne.ie>
2021-11-14 14:44:49 +0000

    new feature

--- hello.py
+++ hello.py
@@ -8,6 +8,7 @@ print("Would you like to:")
 print("1: Check if a year is a leap year?")
 print("2: Print the prime numbers up to a given integer?")
 print("3: Print the Fibonaci sequence to a given integer?")
+print("4: Get the factorial of something")
 ans = int(input("Please enter 1, 2 or 3:"))
 
 while ans != 1 and ans != 2:
@@ -21,3 +22,6 @@ if ans == 2:
 
 if ans == 3:
     fib.print_sequence()
+
+if ans == 4:
+    
