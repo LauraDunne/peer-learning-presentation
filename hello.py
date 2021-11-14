f1fe1ec99671765acef24bd3c057e2e922f60706
Laura Dunne <laura@lauradunne.ie>
2021-11-14 12:55:08 +0000

    adding fibonaci

--- hello.py
+++ hello.py
@@ -1,14 +1,19 @@
 import leap_year
 import primes
+import fib 
 
 print("Hello User, you are looking well today!")
 print("Would you like to:")
 print("1: Check if a year is a leap year?")
 print("2: Print the prime numbers up to a given integer?")
-ans = int(input("Please enter 1 or 2 :"))
+print("3: Print the Fibonaci sequence to a given integer?")
+ans = int(input("Please enter 1, 2 or 3:"))
 
 if ans == 1:
     leap_year.leap_year_function()
 
 if ans == 2:
     primes.print_primes()
+
+if ans == 3:
+    fib.print_sequence()
