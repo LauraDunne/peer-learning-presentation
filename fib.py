90048570e04b66c23507ba9023e5db561ec1088a
Laura Dunne <laura@lauradunne.ie>
2021-11-14 11:22:06 +0000

    adding Fibonaci sequence

---
+++ fib.py
@@ -0,0 +1,20 @@
+
+def print_sequence():
+    num = int(input("Input number here: "))
+
+    print ('Series is: ', 0, ", ", 1, sep = "", end = "")
+
+    i = 2
+    a = 0
+    b = 1
+
+    while i < num:
+        fib = b+a
+            
+        print (',', fib, end="")
+        a = b
+        b = fib
+        i += 1
+
+if __name__ == '__main__':
+    print_sequence()
