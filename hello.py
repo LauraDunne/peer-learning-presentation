df3de88b087ff4442c2d1ac0dfd1f25ff8585bda
Laura Dunne <laura@lauradunne.ie>
2021-11-14 13:01:27 +0000

    adding error handling

--- hello.py
+++ hello.py
@@ -7,6 +7,9 @@ print("1: Check if a year is a leap year?")
 print("2: Print the prime numbers up to a given integer?")
 ans = int(input("Please enter 1 or 2 :"))
 
+while ans != 1 and ans != 2:
+    ans = int(input("Please enter 1 or 2:"))
+
 if ans == 1:
     leap_year.leap_year_function()
 
