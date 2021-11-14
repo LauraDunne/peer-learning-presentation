0c0d61854b9b5ef079bae0145b0a32b523bbe8e4
Laura Dunne <laura@lauradunne.ie>
2021-11-14 12:48:16 +0000

    undoing changes

--- hello.py
+++ hello.py
@@ -1,12 +1,10 @@
 import leap_year
 import primes
-import fib 
 
 print("Hello User, you are looking well today!")
 print("Would you like to:")
 print("1: Check if a year is a leap year?")
 print("2: Print the prime numbers up to a given integer?")
-print("3: Print the Fibonaci sequence to a given integer?")
 ans = int(input("Please enter 1 or 2 :"))
 
 if ans == 1:
@@ -14,6 +12,3 @@ if ans == 1:
 
 if ans == 2:
     primes.print_primes()
-
-if ans == 3:
-    fib.print_sequence()
